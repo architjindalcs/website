977366270361071466566d855335e03d32bab74d
Archit Jindal <architjindal18103019@gmail.com>
2020-04-03 00:07:28 +0530

    Path changes

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,26 +1,26 @@
 console.log("JavaScript, Loaded!!");
 var tv=document.querySelector(".tvimgs")
 var list=
-["url('images/1.jpg')",
-"url('images/2.jpg')",
-"url('images/3.jpg')",
-"url('images/4.jpg')",
-"url('images/5.jpg')",
-"url('images/6.jpg')",
-"url('images/7.jpg')",
-"url('images/8.jpg')",
-"url('images/9.jpg')",
-"url('images/10.jpg')",
-"url('images/11.jpg')",
-"url('images/12.jpg')",
-"url('images/13.jpg')",
-"url('images/14.jpg')",
-"url('images/15.jpg')",
-"url('images/16.jpg')",
-"url('images/17.jpg')",
-"url('images/18.jpg')",
-"url('images/19.jpg')",
-"url('images/20.jpg')"]
+["url('/images/1.jpg')",
+"url('/images/2.jpg')",
+"url('/images/3.jpg')",
+"url('/images/4.jpg')",
+"url('/images/5.jpg')",
+"url('/images/6.jpg')",
+"url('/images/7.jpg')",
+"url('/images/8.jpg')",
+"url('/images/9.jpg')",
+"url('/images/10.jpg')",
+"url('/images/11.jpg')",
+"url('/images/12.jpg')",
+"url('/images/13.jpg')",
+"url('/images/14.jpg')",
+"url('/images/15.jpg')",
+"url('/images/16.jpg')",
+"url('/images/17.jpg')",
+"url('/images/18.jpg')",
+"url('/images/19.jpg')",
+"url('/images/20.jpg')"]
 tv.style.backgroundImage=list[0]
 i=0
 var myinterval=setInterval(function()
@@ -33,7 +33,7 @@ var myinterval=setInterval(function()
     tv.style.backgroundImage=list[i];
 },1500)
 var simg=document.querySelector(".search1 button");
-var neww="url('images/tv.png')";
+var neww="url('/images/tv.png')";
 var body=document.querySelector("body");
 simg.addEventListener("click",function()
 {
